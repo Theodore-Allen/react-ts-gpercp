161dd6502030ec1a92931a26aafba2660630c7e3
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-03-22 12:46:16 -0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,10 +2,6 @@ pokimport * as React from 'react';
 import './style.css';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return null
+
 }
