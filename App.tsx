184d4df56c0a72f978d9cb5936eccbc73b10230f
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-03-29 07:34:32 -0700

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -3,7 +3,25 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
-  fetch('https://pokeapi.co/api/v2/pokemon').then((e) => console.log(e));
+  const [pokimon, setPokimon] = React.useState([]);
+  const [currentPg, setCurrentPg] = React.useState(
+    'https://pokeapi.co/api/v2/pokemon'
+  );
+  const [nextPg, setNexttPg] = React.useState(
+    'https://pokeapi.co/api/v2/pokemon'
+  );
+  const [prevPg, setPrevPg] = React.useState(
+    'https://pokeapi.co/api/v2/pokemon'
+  );
 
-  return null;
+  fetch(currentPg)
+    .then((e) => {
+      return e.json();
+    })
+    .then((e) => {
+      setNexttPg(e.next);
+      setPrevPg(e.previous)
+      setPokimon(e.results.map((e) => e));
+    });
+  return;
 }
