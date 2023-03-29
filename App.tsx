2b83714e6cd565815173eb361de57e01cff5beaf
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-03-29 12:28:04 -0700

    2 files modified

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import PokiList from './PokiList';
 import NextBtn from './nextBtn';
 import './style.css';
 
-
 export default function App() {
   const [pokimon, setPokimon] = React.useState([]);
   const [currentPg, setCurrentPg] = React.useState(
@@ -11,27 +10,38 @@ export default function App() {
   );
   const [nextPg, setNexttPg] = React.useState();
   const [prevPg, setPrevPg] = React.useState();
-function HandleNextPg()
-{
-  
-}
-function HandlePrevPg()
-{}
-  fetch(currentPg)
-    .then((e) => {
-      return e.json();
-    })
-    .then((e) => {
-      setNexttPg(e.next);
-      setPrevPg(e.previous);
-      setPokimon(e.results.map((e) => e));
-    });
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    setLoading(true);
+    fetch(currentPg)
+      .then((e) => {
+        return e.json();
+      })
+      .then((e) => {
+        setNexttPg(e.next);
+        setPrevPg(e.previous);
+        setPokimon(e.results.map((e) => e));
+      });
+    setLoading(false);
+  }, [currentPg]);
+  function HandleNextPg() {
+    setCurrentPg(nextPg);
+  }
+  function HandlePrevPg() {
+    setCurrentPg(prevPg);
+  }
 
-
+  if (loading) {
+    return <h1>Loading..</h1>;
+  }
   return (
     //@ts-expect-error
-  <>
-   <PokiList pokimons={pokimon} />
-   <NextBtn prev={prevPg ? HandlePrevPg : null} next={nextPg ? HandleNextPg: null}/>
-  </>);
+    <>
+      <PokiList pokimons={pokimon} />
+      <NextBtn
+        prev={prevPg ? HandlePrevPg : null}
+        next={nextPg ? HandleNextPg : null}
+      />
+    </>
+  );
 }
--- nextBtn.tsx
+++ nextBtn.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
-export default function NextBtn({prev,next })
-{
-  return<div>{prev && }</>
+export default function NextBtn({ prev, next }) {
+  return (
+    <div>
+      {prev && <button onClick={prev}>prev</button>}
+      {next && <button onClick={next}>Next</button>}
+    </div>
+  );
 }
